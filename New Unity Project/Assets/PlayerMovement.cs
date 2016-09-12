6375272fd4a3982fda849a5d6313a99ac8f5d3e6
Cassidy Barlow <cassidy_barlow@yahoo.com>
2016-09-12 13:48:09 -0600

    First Unity Commit

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovement : MonoBehaviour {
+    //This is the character controller component
+    private CharacterController myCC;
+    // temp var of datatype vector 3 to move the character
+    private Vector3 tempPos;
+    //Speed of the temp var in x
+    public float speed = 1;
+    public float gravity = 1;
+    public float jumpSpeed = 1;
+
+    //used to move player
+
+
+    // Use this for initialization
+    void Start() {
+        //This "finds" the character component. Calls character controller on object
+        myCC = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            tempPos.y = jumpSpeed;
+        }
+
+        tempPos.y -= gravity;
+
+        tempPos.x = speed*Input.GetAxis("Horizontal");
+        //deltaTime slows it to a manageable rate, no matter what the frame rate. Adds consitent time.
+        myCC.Move(tempPos * Time.deltaTime);
+
+    }
+}
